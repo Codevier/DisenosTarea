b89a37208c7b362c185551b4f565efc00c063fa9
Xaavier <xavier.munoz.pisco@gmail.com>
2020-07-24 18:57:33 -0500

    Commmit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,103 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity2 extends AppCompatActivity  implements
+        NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.iconmenu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navView = (NavigationView)findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones,menu);
+
+        MenuItem menuItem=menu.findItem(R.id.action_buscar);
+        SearchView searchView=(SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Buscar en Drive");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+          }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        boolean fragmentTransaction = false;
+
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.menu_seccion_1:
+                fragment = new Fragment1();
+               fragmentTransaction = true;
+                break;
+            case R.id.menu_seccion_2:
+                fragment = new Fragment2();
+                fragmentTransaction = true;
+                break;
+            case R.id.menu_seccion_3:
+                fragment = new Fragment3();
+                fragmentTransaction = true;
+                break;
+
+        }
+
+        if(fragmentTransaction) {
+            getSupportFragmentManager().beginTransaction()
+            .replace(R.id.content_frame, fragment)
+            .commit();
+
+            menuItem.setChecked(true);
+            getSupportActionBar().setTitle(menuItem.getTitle());
+        }
+        drawerLayout.closeDrawers();
+        return true;
+
+    }
+}
